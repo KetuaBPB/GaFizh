1fb6f927e6ab941eed719df87b5b282df0589ab5
ketua <ketuabpb@gmail.com>
2025-08-21 22:56:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
     _.perbaiki(import.meta.url);
     var { ambilFile, async } = _;
 }
-import https from "https";
-https.createServer((q, s) => {
+import http from "http";
+http.createServer((q, s) => {
     s.writeHead(200, { "Content-Type": "text/html" });
     s.end(ambilFile("./frontEnd/beranda.html"));
 }).listen(process.env.PORT || 3000);
